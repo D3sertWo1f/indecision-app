1b49f2d5bd30396e34f9ab2e02ef7b5441ab8054
Adolf RJ <adolfojimenez1601@gmail.com>
2018-08-28 23:16:47 -0500

    Created class Action for button

--- src/app.js
+++ src/app.js
@@ -1,13 +1,30 @@
 class Header extends React.Component {
   render() {
-    return  <p> This is from header </p>
+    return  (
+      <div>
+        <h1>Indecision App</h1>
+        <h2>Put your life in the hands of a computer.</h2>
+      </div>
+    );
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return(
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
   }
 }
 
 const jsx = (
   <div>
     <h1>Title</h1>
-    <Header/>
+    <Header />
+    <Header />
+    <Header />
   </div>
 );
 
