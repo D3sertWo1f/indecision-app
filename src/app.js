67e165242575a3794e9ffeba3b0685676f125809
Adolf RJ <adolfojimenez1601@gmail.com>
2018-08-29 22:32:36 -0500

    created this.state.options, rendered to Options comp

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 class IndecisionApp extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      options: ['Thing One', 'Thing Two', 'Thing Three']
+    }
+  }
+
   render() {
     const title = 'Indecision';
     const subtitle = 'Put your life in the hands of a computer';
-    const options = ['Thing One', 'Thing Two', 'Thing Three']
     return(
       <div>
         <Header title={title} subtitle={subtitle}/>
         <Action />
-        <Options options={options}/>
+        <Options options={this.state.options}/>
         <AddOption />
       </div>
     );
