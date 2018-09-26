c73ae768521384332e19492dfffe83693276b28a
Adolf RJ <adolfo.roman@outlook.com>
2018-09-25 19:34:11 -0500

    Refactored to newSyntax

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,89 +6,81 @@ import Header from './Header'
 
 
 export default class IndecisionApp extends React.Component {
-    constructor(props){
-      super(props)
-      console.log(props)
-      this.state={ 
-        options: props.options
-      }
-      this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
-      this.handlePick = this.handlePick.bind(this)
-      this.handleAddOption = this.handleAddOption.bind(this)
-      this.handleDeleteOption = this.handleDeleteOption.bind(this)
+  state = { 
+    options: []
+  }
+  
+  handleDeleteOptions = () => {
+    this.setState(() => ({options: []}));
+  };
+
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState)=>({
+      options: prevState.options.filter(option => option !== optionToRemove)
+    }))
+  }
+
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length)
+    const option = this.state.options[randomNum]
+    alert(option)
+  }
+
+  handleAddOption = (option) => {
+    if(!option){
+      console.log("error")
+      return 'Please insert a valid string';
+    } else if (this.state.options.indexOf(option) > -1){
+      console.log("error")
+      return 'This option already exists';
     }
 
-    componentDidMount() {
-      try {
-        const json = localStorage.getItem('options');
-        const options = JSON.parse(json);
-  
-        if (options) {
-          this.setState(() => ({ options }));
-        }
-      } 
-      
-      catch (e) {
-        // Do nothing at all
-      }
-    }
+    this.setState((prevState) => ({options: prevState.options.concat([option])}));
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-      if (prevState.options.length !== this.state.options.length) {
-        const json = JSON.stringify(this.state.options);
-        localStorage.setItem('options', json);
-      }
-    }
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
 
-    componentWillUnmount() {
-      console.log('componentWillUnmount');
-    }
-  
-    handleDeleteOptions(){
-      this.setState(() => ({options: []}));
-    };
-  
-    handleDeleteOption(optionToRemove){
-      this.setState((prevState)=>({
-        options: prevState.options.filter(option => option !== optionToRemove)
-      }))
-    }
-  
-    handlePick(){
-      const randomNum = Math.floor(Math.random() * this.state.options.length)
-      const option = this.state.options[randomNum]
-      alert(option)
-    }
-  
-    handleAddOption(option){
-      if(!option){
-        console.log("error")
-        return 'Please insert a valid string';
-      } else if (this.state.options.indexOf(option) > -1){
-        console.log("error")
-        return 'This option already exists';
+      if (options) {
+        this.setState(() => ({ options }));
       }
-  
-      this.setState((prevState) => ({options: prevState.options.concat([option])}));
-    }
-  
-    render() {
-      console.log(this.state.options)
-      const subtitle = 'Put your life in the hands of a computer';
-      return(
-        <div>
-          <Header  subtitle={subtitle}/>
-          <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick}/>
-          <Options
-            options={this.state.options}
-            handleDeleteOptions={this.handleDeleteOptions}
-            handleDeleteOption={this.handleDeleteOption}
-          />
-          <AddOption handleAddOption={this.handleAddOption}/>
-        </div>
-      );
+    } 
+    
+    catch (e) {
+      // Do nothing at all
     }
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  render() {
+    console.log(this.state.options)
+    const subtitle = 'Put your life in the hands of a computer';
+    return(
+      <div>
+        <Header  subtitle={subtitle}/>
+        <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick}/>
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption handleAddOption={this.handleAddOption}/>
+      </div>
+    );
+  }
+}
   
   IndecisionApp.defaultProps = {
     options: []
