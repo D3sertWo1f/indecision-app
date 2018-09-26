36bc8e29bf8565bf1938f7429566dc3a3c7fc715
Adolf RJ <adolfo.roman@outlook.com>
2018-09-25 19:07:53 -0500

    Add lifecycles

---
+++ src/components/Options.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Option from './Option';
+
+const Options = (props) => {
+    return(
+      <div>
+        <button onClick={props.handleDeleteOptions}>Remove All</button>
+        {
+          props.options.map((option, index) => (
+            <Option
+              optionText={option}
+              key={index}
+              handleDeleteOption = {props.handleDeleteOption}
+            />
+          ))
+        }
+      </div>
+    );
+  }
+
+  export default Options;
